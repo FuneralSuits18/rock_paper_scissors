34e933581d5374214f4f5ee25560cc8462058884
FuneralSuits18 <50417827+FuneralSuits18@users.noreply.github.com>
2023-02-28 19:02:56 +0600

    Complete the ui
    Animations and transitions still left.

--- index.js
+++ index.js
@@ -1,3 +1,38 @@
+const container = document.querySelector('.container');
+const play_btn = document.querySelector('.play');
+play_btn.addEventListener('click', loadGame);
+
+function loadGame(){
+  const game_container = document.querySelector('.game_container');
+  container.removeChild(play_btn);
+  game_container.hidden = false;
+}
+
+const btn_play_again = document.createElement('button');
+btn_play_again.classList.add('btn');
+btn_play_again.textContent = 'Play again!';
+btn_play_again.addEventListener('click', playAgain);
+
+function playAgain(){
+  round_count = 0;
+  no_of_player_wins = 0;
+  no_of_computer_wins = 0;
+  no_of_draws = 0;
+
+  round_result.textContent = '';
+  wins.textContent = 'Wins : 0';
+  draws.textContent = 'Draws : 0';
+  losses.textContent = 'Losses : 0';
+  game_result.textContent = '';
+
+  btn_container.removeChild(btn_play_again);
+  btn_container.appendChild(btn_rock);
+  btn_container.appendChild(btn_paper);
+  btn_container.appendChild(btn_scissors);
+  
+}
+
+
 let no_of_player_wins = 0;
 let no_of_computer_wins = 0;
 let no_of_draws = 0;
@@ -6,10 +41,11 @@ const btn_rock = document.querySelector('.btn_rock');
 const btn_paper = document.querySelector('.btn_paper');
 const btn_scissors = document.querySelector('.btn_scissors');
 
-const round_result = document.createElement('div');
-round_result.classList.add('round_result');
+const round_result = document.querySelector('.round_result');
 const game_result = document.createElement('div');
 game_result.classList.add('game_result');
+game_result.style.paddingTop = '2rem';
+
 const game_record = document.createElement('ul');
 const wins = document.createElement('li');
 const draws = document.createElement('li');
@@ -22,32 +58,47 @@ game_record.appendChild(draws);
 game_record.appendChild(losses);
 
 btn_rock.addEventListener('click', playRoundRock);
-btn_paper.addEventListener('click', playRoundScissors);
+btn_paper.addEventListener('click', playRoundPaper);
 btn_scissors.addEventListener('click', playRoundScissors);
 
 let round_count = 0;
 function playRoundRock(){
-  if(round_count == 5){
+  if(round_count == 4){
+    playRound('paper', getComputerChoice());
+    btn_container.removeChild(btn_rock);
+    btn_container.removeChild(btn_paper);
+    btn_container.removeChild(btn_scissors);
+    btn_container.appendChild(btn_play_again);
     return;
   }
   playRound('rock', getComputerChoice());
 }
+const btn_container = document.querySelector('.btn_container');
 function playRoundPaper(){
-  if(round_count == 5){
+  if(round_count == 4){
+    playRound('paper', getComputerChoice());
+    btn_container.removeChild(btn_rock);
+    btn_container.removeChild(btn_paper);
+    btn_container.removeChild(btn_scissors);
+    btn_container.appendChild(btn_play_again);
     return;
   }
   playRound('paper', getComputerChoice());
 }
 function playRoundScissors(){
-  if(round_count == 5){
+  if(round_count == 4){
+    playRound('paper', getComputerChoice());
+    btn_container.removeChild(btn_rock);
+    btn_container.removeChild(btn_paper);
+    btn_container.removeChild(btn_scissors);
+    btn_container.appendChild(btn_play_again);
     return;
   }
   playRound('scissors', getComputerChoice());
 }
 
-const container = document.querySelector('.container');
-container.appendChild(round_result);
-container.appendChild(game_record);
+const game_status = document.querySelector('.game_status');
+game_status.appendChild(game_record);
 
 function getComputerChoice() {
   let randomNum = Math.floor(Math.random() * 3) + 1;
@@ -95,19 +146,13 @@ function playRound(playerSelection, computerSelection) {
   if(no_of_player_wins + no_of_computer_wins + no_of_draws == 5){
     if (no_of_player_wins > no_of_computer_wins) {
       game_result.textContent = `You won the game! You've won ${no_of_player_wins} and drawn ${no_of_draws} out of 5 rounds!`;
-      container.appendChild(game_result);
+      game_status.appendChild(game_result);
     } else if (no_of_computer_wins > no_of_player_wins) {
-      game_result.textContent = `You lost. You've won ${no_of_player_wins} and drawn ${no_of_draws} out of 5 rounds`;
-      container.appendChild(game_result);
+      game_result.textContent = `You lost the game. You've won ${no_of_player_wins} and drawn ${no_of_draws} out of 5 rounds`;
+      game_status.appendChild(game_result);
     } else {
       game_result.textContent = `Game Draw! You've won ${no_of_player_wins} rounds, lost ${no_of_computer_wins} and drawn ${no_of_draws} out of 5 rounds`;
-      container.appendChild(game_result);
+      game_status.appendChild(game_result);
     }
   }
-}
-
-if(no_of_player_wins + no_of_computer_wins + no_of_draws >= 5){
-  btn_rock.removeEventListener('click', playRoundRock);
-  btn_paper.removeEventListener('click', playRoundPaper);
-  btn_scissors.removeEventListener('click', playRoundScissors);
 }
